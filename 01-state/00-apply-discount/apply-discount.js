299fb017c0a0e33b6f70acd157aac80e88bd39ed
marcelaorellana <marcelapaz.1985@gmail.com>
2017-10-26 19:54:20 -0300

    arreglos finales

--- 01-state/00-apply-discount/apply-discount.js
+++ 01-state/00-apply-discount/apply-discount.js
@@ -1,18 +1,14 @@
 'use strict';
 
 
-// Broken: returns new array but mutates same object references.
 const applyDiscount = (cart, discount) => {
-	const result = Object.assign([], cart);
-
-  if (!result.length) {
+	if (!cart.length) {
     return [];
   }
-
-  const current = result.shift();
-  current.price *= (1 - discount);
-
-  return [current].concat(applyDiscount(result, discount));
+  return [{
+      price: cart[0].price * (1 - discount)
+    }]
+    .concat(applyDiscount(cart.slice(1), discount));
 };
 
 module.exports = applyDiscount;
